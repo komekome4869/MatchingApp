87c6b5b1725a745debfa37f7edd4f9143f32146b
komekome4869 <hikaru30224869@gmail.com>
2021-07-19 04:23:35 +0900

    a

--- client/Client.java
+++ client/Client.java
@@ -199,7 +199,7 @@ public class Client extends JFrame implements ActionListener,ChangeListener{
 	static ObjectInputStream ois;
 	static BufferedWriter bw;
 	static Object inputObj;
-	String ipAddress = "localhost";	//ipアドレス設定
+	String ipAddress = "182.170.133.46";	//ipアドレス設定
 	int port = 50;  //port番号設定
 	String inputLine = "0";
 
