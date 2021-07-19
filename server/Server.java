0335337b012430b5599f68263f0a0da80e716f5a
iWori <iWori@192.168.10.101>
2021-07-20 03:40:24 +0900

    jAGを修正

--- server/Server.java
+++ server/Server.java
@@ -1334,7 +1334,6 @@ public class Server extends JFrame implements ActionListener{
 		FileWriter fw = null;
 
         String line = "";
-        String students[] = null;
         StringBuffer strbuf = new StringBuffer("");
 
         try {
@@ -1350,15 +1349,15 @@ public class Server extends JFrame implements ActionListener{
 				strbuf.append(line + "\n");
 			}
 
-			if(line.length() > 3) {
-				students = line.split(" ");//TODO
-			}
+			String students[] = line.split(" ");//TODO
 			strbuf.append(line + "\n");
 
 			//非ホストユーザがグループに入っているか確認
 			int count_true = 0;
 			for(int i=0; i<students.length; i++) {
-				if(judgeJoinedGroup(students[i], uuid)) count_true++;
+				if(students[i].length() > 0)
+					if(judgeJoinedGroup(students[i], uuid))
+						count_true++;
 			}
 
 			//該当行を検索
@@ -1599,9 +1598,10 @@ public class Server extends JFrame implements ActionListener{
 		String Groups[] = new String[100]; //参加してるグループ、とりあえず100個まで
 		String InvitedGroups[] = new String[100]; //誘われてるグループ、とりあえず100個まで
 
+		//ファイルを読み込み
+		File file = new File(System.getProperty("user.dir") + "\\ID\\" + studentNum + ".txt");
+
 			try {
-				//ファイルを読み込み
-				File file = new File(System.getProperty("user.dir") + "\\ID\\" + studentNum + ".txt");
 				fr = new FileReader(file);
 				br = new BufferedReader(fr);
 				int line_counter = 0;
@@ -1616,7 +1616,8 @@ public class Server extends JFrame implements ActionListener{
 				SentGoodStudents = line.split(" ");
 				//ここでいいね削除
 				for(int i=0; i<SentGoodStudents.length; i++) {
-					deleteReceivedGood(SentGoodStudents[i], studentNum);
+					if(SentGoodStudents[i].length() > 0)
+						deleteReceivedGood(SentGoodStudents[i], studentNum);
 				}
 
 				//次の行
@@ -1626,7 +1627,8 @@ public class Server extends JFrame implements ActionListener{
 				BeingSentGoodStudents = line.split(" ");
 				//ここでいいね削除
 				for(int i=0; i<BeingSentGoodStudents.length; i++) {
-					deleteGood(BeingSentGoodStudents[i], studentNum);
+					if(BeingSentGoodStudents[i].length() > 0)
+						deleteGood(BeingSentGoodStudents[i], studentNum);
 				}
 
 				//次の行
@@ -1635,7 +1637,8 @@ public class Server extends JFrame implements ActionListener{
 				//空白で分割して保存、マッチングした人
 				MatchingStudents = line.split(" ");
 				for(int i=0; i<MatchingStudents.length; i++) {
-					deleteMatching(MatchingStudents[i], studentNum, false);
+					if(MatchingStudents[i].length() > 0)
+						deleteMatching(MatchingStudents[i], studentNum, false);
 				}
 
 				//次の行
@@ -1644,7 +1647,8 @@ public class Server extends JFrame implements ActionListener{
 				//空白で分割して保存、参加してるグループ
 				Groups = line.split(" ");
 				for(int i=0; i<Groups.length; i++) {
-					deleteGroup(Groups[i]);
+					if(Groups[i].length() > 0)
+						deleteGroup(Groups[i]);
 				}
 
 				//次の行
@@ -1653,16 +1657,9 @@ public class Server extends JFrame implements ActionListener{
 				//空白で分割して保存、誘われてるグループ
 				InvitedGroups = line.split(" ");
 				for(int i=0; i<InvitedGroups.length; i++) {
-					deleteGroup(InvitedGroups[i]);
+					if(InvitedGroups[i].length() > 0)
+						deleteGroup(InvitedGroups[i]);
 				}
-
-				//削除
-				file.delete();
-				image_user_dir.delete();
-
-				//再度読み込み
-				readAllUserFiles();
-				readAllGroupFiles();
 
 			}catch(IOException e) {
 				System.err.print("ユーザ削除に関する処理でエラーが発生しました：" + e);
@@ -1671,6 +1668,17 @@ public class Server extends JFrame implements ActionListener{
 				try {
 					fr.close();
 					br.close();
+
+					//削除
+					if(file.delete()) System.out.println("削除成功");
+					else System.out.println("削除失敗");
+					image_user_dir.delete();
+
+					System.out.println(studentNum + "を削除しました");
+
+					//再度読み込み
+					readAllUserFiles();
+					readAllGroupFiles();
 				} catch (IOException e) {
 					e.printStackTrace();
 				}
@@ -1927,7 +1935,7 @@ public class Server extends JFrame implements ActionListener{
  			br = new BufferedReader(fr);
  			String line;
 			int line_counter = 0;
-			
+
 			System.out.println(file);
 
 			//該当行を検索
@@ -1949,7 +1957,7 @@ public class Server extends JFrame implements ActionListener{
 			while((line = br.readLine()) != null) {
 				strbuf.append(line + "\n");
 			}
-			
+
 
 			//書き込み
 			fw = new FileWriter(file);
@@ -1964,7 +1972,7 @@ public class Server extends JFrame implements ActionListener{
    			return false;
    		}finally {
    			try {
-   				//fw.close();
+   				fw.close();
 				br.close();
 			} catch (IOException e) {
 				// TODO 自動生成された catch ブロック
@@ -2380,7 +2388,6 @@ public class Server extends JFrame implements ActionListener{
 		            try {
 						//ファイルを読み込み
 						file = fileList[i];
-						System.out.println(file);
 						if(file.isDirectory()) continue;
 						fr = new FileReader(file);
 						br = new BufferedReader(fr);
@@ -2393,7 +2400,6 @@ public class Server extends JFrame implements ActionListener{
 								if(line.contains("0")) {
 									notAuthentificatededUsers[j] = new File(file.toString());
 									pageAuthen = j;
-							    	System.out.println("ok");
 									j++;
 								}
 								break;
@@ -2587,12 +2593,41 @@ public class Server extends JFrame implements ActionListener{
 					}
 			}
 			else if(cmd.equals("却下")) {
+			 	BufferedReader br = null;
+		        FileReader fr = null;
+
 				if(pageAuthen == -1) {
 					this.setVisible(false);
 					this.dispose();
 					//TODO 認証ウインドウだけ閉じたい。間違ってる可能性が高い
 				}
 				else {
+					File file = notAuthentificatededUsers[pageAuthen];
+			        try {
+						//ファイルを読み込み
+						fr = new FileReader(file);
+						br = new BufferedReader(fr);
+
+					}catch(IOException e) {
+						System.err.print("認証に関する処理でエラーが発生しました：" + e);
+					}finally {
+						try {
+							fr.close();
+							br.close();
+							//削除
+							if(file.delete()) System.out.println("削除成功");
+							else System.out.println("削除失敗");
+						} catch (IOException e) {
+							// TODO 自動生成された catch ブロック
+							e.printStackTrace();
+						}
+					}
+					pageAuthen--;
+					if(pageAuthen == -1) {
+						this.setVisible(false);
+						this.dispose();
+						//TODO 認証ウインドウだけ閉じたい。間違ってる可能性が高い
+					}
 					nextPage();
 				}
 			}
